2409ad4a2fe0f30bfe2c5f380672e7fe9c8c7ee1
Shubham Soni <shubham.soni@jungleworks.co>
2018-08-05 21:51:11 +0530

    first commit

---
+++ app/src/main/java/com/dexolabs/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.dexolabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dexolabs.c.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
