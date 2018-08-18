41305d2fc8be0897e2e58a413d4aa4b4578a4fca
Shubham Soni <shubham.soni@jungleworks.co>
2018-08-19 02:52:29 +0530

    26 programs

--- app/src/main/java/com/dexolabs/cprogramming/data/ProgramData.java
+++ app/src/main/java/com/dexolabs/cprogramming/data/ProgramData.java
@@ -12,23 +12,178 @@ public class ProgramData {
 
         options.put("Hello world Program", "1_program.html");
         options.put("Print an Integer (Entered by the User)", "2_program.html");
-        options.put("Program to find armstrong number", "2_program.html");
-        options.put("Flowcharts", "2_program.html");
-        options.put("Before C", "2_program.html");
+        options.put("Add Two Integers", "3_program.html");
+        options.put("Multiply two Floating Point Numbers", "4_program.html");
+        options.put("Perform addition, subtraction, multiplication and division", "5_program.html");
+        options.put("Find the Size of int, float, double and char", "9_program.html");
+        options.put("", "10_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
         programModelList.add(new TutorialModel("Basic Programs", options));
 
 
         options = new TreeMap<>();
-        options.put("What is C?", "2_program.html");
-        options.put("Structure of C", "2_program.html");
-        options.put("Compilation", "2_program.html");
-        options.put("Nomenclature", "2_program.html");
-        options.put("DataTypes", "2_program.html");
-        options.put("Operators", "2_program.html");
-        options.put("Qualifiers", "2_program.html");
-        options.put("Output", "2_program.html");
-        options.put("Input", "2_program.html");
-        programModelList.add(new TutorialModel("Overview", options));
+        options.put("Check if a given Integer is Odd or Even", "6_program.html");
+        options.put("Swap Two Numbers", "10_program.html");
+        options.put("Swap Number Without Using Temporary Variables", "11_program.html");
+        options.put("Generate Multiplication Table", "12_program.html");
+        options.put("Find Factorial of a Number", "13_program.html");
+        options.put("Calculate the Sum of Natural Numbers", "14_program.html");
+        options.put(" Whether a Number is Positive or Negative", "16_program.html");
+        options.put("Check Leap Year", "17_program.html");
+        options.put("Largest Number Among Three Numbers", "18_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
+        options.put("", "5_program.html");
+        programModelList.add(new TutorialModel("Numbers", options));
+
+
+        options = new TreeMap<>();
+        options.put("Find ASCII Value of a Character", "7_program.html");
+        options.put("Program to check vowel or consonant using if else", "5_program.html");
+        options.put("Whether a Character is an Alphabet or not", "15_program.html");
+        options.put("Check Whether a Character is Vowel or Consonant", "19_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        programModelList.add(new TutorialModel("Characters", options));
+
+        options = new TreeMap<>();
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        programModelList.add(new TutorialModel("Arrays", options));
+
+
+        options = new TreeMap<>();
+        options.put("Frequency of Characters in a String", "20_program.html");
+        options.put("Count the number of vowels, consonants", "21_program.html");
+        options.put("Remove all Characters in a String Except Alphabet", "22_program.html");
+        options.put("Find the Length of a String", "23_program.html");
+        options.put("Concatenate Two Strings", "24_program.html");
+        options.put("Copy String Without Using strcpy()", "25_program.html");
+        options.put("Sort Elements in Dictionary Order", "26_program.html");
+        options.put("", "27_program.html");
+        options.put("", "2_program.html");
+        programModelList.add(new TutorialModel("Strings", options));
+
+
+        options = new TreeMap<>();
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        programModelList.add(new TutorialModel("Matrix", options));
+
+
+        options = new TreeMap<>();
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        programModelList.add(new TutorialModel("Patterns", options));
+
+
+        options = new TreeMap<>();
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        programModelList.add(new TutorialModel("Pointers I/O", options));
+
+
+        options = new TreeMap<>();
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        programModelList.add(new TutorialModel("Structure and Union", options));
+
+
+        options = new TreeMap<>();
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        programModelList.add(new TutorialModel("File I/O", options));
+
+
+        options = new TreeMap<>();
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        programModelList.add(new TutorialModel("Conversations", options));
+
+
+        options = new TreeMap<>();
+        options.put("Use gets() function", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        options.put("", "2_program.html");
+        programModelList.add(new TutorialModel("Extras", options));
 
         return programModelList;
     }
