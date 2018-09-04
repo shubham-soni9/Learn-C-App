a39403cf7c7006e5f9aa3ea50e2640a961bb14b7
Shubham Soni <shubham.soni@jungleworks.co>
2018-09-04 20:03:21 +0530

    diff 3

--- app/src/main/java/com/dexolabs/cprogramming/data/DifferenceData.java
+++ app/src/main/java/com/dexolabs/cprogramming/data/DifferenceData.java
@@ -6,6 +6,8 @@ public class DifferenceData {
     public static LinkedHashMap<String, String> getDifferencesData() {
         LinkedHashMap<String, String> options = new LinkedHashMap<>();
         options.put("C v/s C++", "diff_1.html");
+        options.put("C v/s Java", "diff_2.html");
+        options.put("Procedure Oriented v/s Object Oriented Programming", "diff_3.html");
         return options;
     }
 }
